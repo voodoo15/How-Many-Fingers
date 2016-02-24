037de4ba9192f22dbc57e850254c092c0e71114c
voodoo15 <arnold.redoblado@gmail.com>
2016-02-24 00:48:42 -0500

    How Many Fingers App
    Guess a number from 0 to 5.  Number is randomized.  If number is > 5,
    then tell the user.

--- How Many Fingers/ViewController.swift
+++ How Many Fingers/ViewController.swift
@@ -10,16 +10,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lblResponse: UILabel!
+    
+    @IBOutlet var txtGuess: UITextField!
+    
+    @IBOutlet var imgResult: UIImageView!
+    
+    @IBAction func btnSubmit(sender: AnyObject) {
+        
+        //Randomize a number
+        let iRandom = Int(arc4random_uniform(6))
+        print(iRandom)
+        
+        //If the guess = random number, success
+        //else display wrong number.  If number is > 5
+        //tell them they're stupid.
+        if iRandom == Int(txtGuess.text!) {
+            imgResult.image = UIImage(named: "Salute_Finger.jpg")
+            lblResponse.text = "Damnit, you're psychic!"
+            print("Correct")
+            }
+        else if Int(txtGuess.text!) > 5 {
+            imgResult.image = UIImage(named: "really.jpg")
+            print("Too many fingers")
+            lblResponse.text = "REALLY? Try again...idiot!"
+            }
+        else {
+            imgResult.image = UIImage(named: "wrong.png")
+            print("Wrong answer")
+            lblResponse.text = "Nope, try again!"
+            }
+        
+        txtGuess.text = ""
+        dismissKeyboard()
+    }
+    
+    //Calls this function when the tap is recognized.
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    
 }
 
